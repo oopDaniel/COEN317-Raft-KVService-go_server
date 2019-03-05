558ecd330fed2f54457e3d7eb6dc8f03fde32a60
Daniel <oopdaniel@gmail.com>
2019-03-04 18:43:22 -0800

    implement mock server

--- go.mod
+++ go.mod
@@ -1,9 +1,18 @@
 module github.com/oopDaniel/COEN317-Raft-KVService-go_server
 
 require (
+	github.com/cweill/gotests v1.5.2
+	github.com/davidrjenni/reftools v0.0.0-20180914123528-654d0ba4f96d
 	github.com/gorilla/mux v1.7.0
+	github.com/haya14busa/goplay v1.0.0
+	github.com/keegancsmith/rpc v1.1.0
+	github.com/kisielk/gotool v1.0.0
 	github.com/mdempsky/gocode v0.0.0-20190203001940-7fb65232883f
 	github.com/pkg/errors v0.8.1
+	github.com/skratchdot/open-golang v0.0.0-20190104022628-a2dfa6d0dab6
 	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518
+	github.com/stamblerre/gocode v0.0.0-20190213022308-8cc90faaf476
+	golang.org/x/lint v0.0.0-20190301231843-5614ed5bae6f
+	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd
 	golang.org/x/tools v0.0.0-20190304223419-d56157ae751d
 )
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Message struct {
+	Msg string
+}
+
+type ReplyValue struct {
+	Value string
+}
+
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+type Server struct {
+	kv          map[string]string // mock k/v map
+	machines    []string          // mock AWS instances
+	machinesMap map[string]bool
+	alive       map[string]bool
+}
+
+func main() {
+	// initClerk()
+	startServer()
+}
+
+// TODO: use kv service
+// func initClerk() {
+// 	ends := make([]*labrpc.ClientEnd, 5)
+// 	ck := kvraft.MakeClerk(ends)
+// 	fmt.Println(ck)
+// }
+
+func startServer() {
+	sv := Server{
+		kv:          make(map[string]string),
+		machines:    []string{"A", "B", "C", "D", "E"},
+		machinesMap: map[string]bool{"A": true, "B": true, "C": true, "D": true, "E": true},
+		alive:       map[string]bool{"A": true, "B": true, "C": true, "D": true, "E": true},
+	}
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/state", sv.getState).Methods("GET").Queries("key", "{key}")
+	router.HandleFunc("/state", sv.setState).Methods("POST")
+	router.HandleFunc("/machines/all", sv.getMachineByAction("all")).Methods("GET")
+	router.HandleFunc("/machines/alive", sv.getMachineByAction("alive")).Methods("GET")
+	router.HandleFunc("/machine/{id}", sv.setMachine).Methods("DELETE")
+	router.HandleFunc("/machine/{id}", sv.setMachine).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func (sv *Server) getState(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	key := params["key"]
+	value, ok := sv.kv[key]
+	if ok {
+		json.NewEncoder(w).Encode(ReplyValue{value})
+	} else {
+		json.NewEncoder(w).Encode(ReplyValue{""})
+	}
+}
+
+func (sv *Server) setState(w http.ResponseWriter, r *http.Request) {
+	var data KeyValue
+	err := json.NewDecoder(r.Body).Decode(&data)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(Message{"Error decoding"})
+	} else {
+		sv.kv[data.Key] = data.Value
+		json.NewEncoder(w).Encode(Message{"Done"})
+	}
+}
+
+func (sv *Server) getMachineByAction(category string) func(http.ResponseWriter, *http.Request) {
+	switch category {
+	case "all":
+		return func(w http.ResponseWriter, r *http.Request) {
+			json.NewEncoder(w).Encode(sv.machines)
+		}
+	case "alive":
+		return func(w http.ResponseWriter, r *http.Request) {
+			alive := make([]string, 0, len(sv.machines))
+			for _, machine := range sv.machines {
+				if isAlive := sv.alive[machine]; isAlive {
+					alive = append(alive, machine)
+				}
+			}
+			json.NewEncoder(w).Encode(alive)
+		}
+	default:
+		return func(w http.ResponseWriter, r *http.Request) {}
+	}
+}
+
+func (sv *Server) setMachine(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	if exist := sv.machinesMap[id]; exist {
+		if r.Method == "DELETE" {
+			sv.alive[id] = false
+		} else {
+			sv.alive[id] = true
+		}
+	}
+	json.NewEncoder(w).Encode(Message{"Done"})
+}
